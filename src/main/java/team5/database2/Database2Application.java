ac8a416f0f2e233e75908d8271e0da31b362abf4
e1b22101 <e1b22101@oit.ac.jp>
2024-10-22 13:40:00 +0900

    リポジトリの初期化

---
+++ src/main/java/team5/database2/Database2Application.java
@@ -0,0 +1,13 @@
+package team5.database2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Database2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Database2Application.class, args);
+	}
+
+}
