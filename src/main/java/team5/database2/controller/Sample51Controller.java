77653296cfd75796f2d4ebe59e1ca13264cc3e33
e1b22057 <e1b22057@oit.ac.jp>
2024-10-22 14:41:38 +0900

    果物のレコードを作成・表示

---
+++ src/main/java/team5/database2/controller/Sample51Controller.java
@@ -0,0 +1,54 @@
+package team5.database2.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import team5.database2.model.Fruit;
+import team5.database2.model.FruitMapper;
+
+/**
+ * /sample5へのリクエストを扱うクラス authenticateの設定をしていれば， /sample5へのアクセスはすべて認証が必要になる
+ */
+@Controller
+@RequestMapping("/sample5")
+public class Sample51Controller {
+
+  @Autowired
+  FruitMapper fMapper;
+
+  @GetMapping("step1")
+  public String sample51() {
+    return "sample51.html";
+  }
+
+  @GetMapping("step2")
+  public String sample52(ModelMap model) {
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
+
+  @GetMapping("step3")
+  @Transactional
+  public String sample53(@RequestParam Integer id, ModelMap model) {
+    // 削除対象のフルーツを取得
+    Fruit fruit3 = fMapper.selectById(id);
+    model.addAttribute("fruit3", fruit3);
+
+    // 削除
+    fMapper.deleteById(id);
+
+    // 削除後のフルーツリストを取得
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
+}
