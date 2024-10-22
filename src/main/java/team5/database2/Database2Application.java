dd739eb2ff5dda7657ce784b728e3d08a7c2e259
e1b22101 <e1b22101@oit.ac.jp>
2024-10-22 15:35:36 +0900

    カウントアップの実装

--- src/main/java/team5/database2/Database2Application.java
+++ src/main/java/team5/database2/Database2Application.java
@@ -2,7 +2,11 @@ package team5.database2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Database2Application {
 
